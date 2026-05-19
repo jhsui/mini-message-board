22a3fce1957e5bf3c1208c839928307909a95eb7
jhsui <jiahansui1998@gmail.com>
2026-05-19 14:35:24 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+import express from "express";
+import path from "node:path";
+
+const app = express();
+
+const __dirname = import.meta.dirname;
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/new", (req, res) => {
+  res.render("form");
+});
+
+app.post("/new", (req, res) => {
+  const messageText = req.body.messageText;
+  const messageUser = req.body.messageUser;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
+});
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+const hostname = "localhost";
+const PORT = 8080;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+
+  console.log(
+    `mini-message-board -- Server running at http://${hostname}:${PORT}/`,
+  );
+});
